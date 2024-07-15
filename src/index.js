b13495011f102dd599257fb9c6e6f9abd83bde13
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2024-07-15 12:42:05 +0300

    adds index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('create-task-form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    listItems(e.target.children[1].value)
+  })
+})
+
+function listItems(items){
+  let li = document.createElement('li');
+  li.textContent = `${items}`
+  document.getElementById('tasks').appendChild(li)
+}
